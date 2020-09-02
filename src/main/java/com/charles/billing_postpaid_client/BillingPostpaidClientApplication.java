0dcd2ecb16034d11f0048f42d36e5e2660b969f2
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 12:58:02 +0700

    [Charles] add initial spring boot project

---
+++ src/main/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplication.java
@@ -0,0 +1,13 @@
+package com.charles.billing_postpaid_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingPostpaidClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingPostpaidClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.billing_postpaid_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPostpaidClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
