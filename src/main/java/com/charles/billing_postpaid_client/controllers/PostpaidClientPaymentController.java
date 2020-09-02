013fb0ffaa4e14e88556044d02d61529a0eddda9
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 23:22:00 +0700

    [Charles] modify payment controller to use biller repository to update bill status

--- src/main/java/com/charles/billing_postpaid_client/controllers/PostpaidClientPaymentController.java
+++ src/main/java/com/charles/billing_postpaid_client/controllers/PostpaidClientPaymentController.java
@@ -1,12 +1,16 @@
 package com.charles.billing_postpaid_client.controllers;
 
+import com.charles.billing_postpaid_client.models.Bill;
 import com.charles.billing_postpaid_client.models.PaymentRequest;
 import com.charles.billing_postpaid_client.models.PaymentResponse;
+import com.charles.billing_postpaid_client.repositories.BillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RestController
@@ -14,16 +18,37 @@ import java.util.UUID;
 public class PostpaidClientPaymentController {
 
     public static final String SUCCESS = "success";
+    public static final String PAID = "paid";
+
+    @Autowired
+    private BillRepository billRepository;
 
     @PostMapping("/")
-    public PaymentResponse doPayment(@RequestBody PaymentRequest paymentRequest) {
-        PaymentResponse response = new PaymentResponse();
-        response.setPaymentId(UUID.randomUUID());
-        response.setBillerId(paymentRequest.getBillerId());
-        response.setCustomerAccountId(paymentRequest.getCustomerAccountId());
-        response.setStatus(SUCCESS);
-        response.setTotalAmount(70000.00);
-        return response;
+    public PaymentResponse doPayment(@RequestBody PaymentRequest paymentRequest) throws Throwable {
+        try {
+            Bill bill = new Bill();
+            bill.setBillerId(paymentRequest.getBillerId());
+            bill.setCustomerAccountId(paymentRequest.getCustomerAccountId());
+            bill.setPaymentDate(LocalDate.now());
+            bill.setPaymentId(UUID.randomUUID());
+            bill.setDueDate(paymentRequest.getDueDate());
+            bill.setTotalAmount(paymentRequest.getTotalAmount());
+            bill.setStatus(PAID);
+            Bill billResponse = billRepository.save(bill);
+
+            PaymentResponse response = new PaymentResponse();
+            response.setPaymentId(billResponse.getPaymentId());
+            response.setBillerId(billResponse.getBillerId());
+            response.setCustomerAccountId(billResponse.getCustomerAccountId());
+            response.setStatus(SUCCESS);
+            response.setTotalAmount(billResponse.getTotalAmount());
+            response.setPaymentDate(billResponse.getPaymentDate());
+
+            return response;
+
+        } catch (Exception e) {
+            throw e.getCause();
+        }
     }
 
 }
