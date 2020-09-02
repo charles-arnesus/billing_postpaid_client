a91296b14c2f60e7202007b5251ee80a9dc7f958
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 13:05:09 +0700

    [Charles] register billing_postpaid_client into eureka server

--- src/main/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplication.java
+++ src/main/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplication.java
@@ -2,8 +2,10 @@ package com.charles.billing_postpaid_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BillingPostpaidClientApplication {
 
 	public static void main(String[] args) {
