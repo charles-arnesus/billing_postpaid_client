9e8e09f09d3d181b73dc17df15230693d6ae0fbb
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-03 13:38:03 +0700

    [Charles] add Dockerfile and eureka properties

--- src/test/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplicationTests.java
+++ src/test/java/com/charles/billing_postpaid_client/BillingPostpaidClientApplicationTests.java
@@ -3,11 +3,11 @@ package com.charles.billing_postpaid_client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BillingPostpaidClientApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class BillingPostpaidClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
